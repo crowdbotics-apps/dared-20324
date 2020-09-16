3f64eb7f58decef71323872aeada22e1b603194d
Crowdbotics <team@crowdbotics.com>
2020-09-16 03:30:48 +0000

    add package blueprint-react-native-tutorial-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Tutorial7101999', human_name: 'Tutorial 7', access_route: 'Tutorial7101999', icon: 'question-circle'},
 { name: 'SignUp19101998', human_name: 'Sign Up 1 9', access_route: 'SignUp19101998'},
 { name: 'SignUp110101997', human_name: 'Sign Up 1 10', access_route: 'SignUp110101997'},
 { name: 'Camera9101996', human_name: 'Camera 9', access_route: 'Camera9101996', icon: 'camera'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Tutorial7101999Navigator from '../features/Tutorial7101999/navigator';
 import SignUp19101998Navigator from '../features/SignUp19101998/navigator';
 import SignUp110101997Navigator from '../features/SignUp110101997/navigator';
 import Camera9101996Navigator from '../features/Camera9101996/navigator';
@@ -18,6 +19,7 @@ import BlankScreen0101994Navigator from '../features/BlankScreen0101994/navigato
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Tutorial7101999: { screen: Tutorial7101999Navigator },
 SignUp19101998: { screen: SignUp19101998Navigator },
 SignUp110101997: { screen: SignUp110101997Navigator },
 Camera9101996: { screen: Camera9101996Navigator },
