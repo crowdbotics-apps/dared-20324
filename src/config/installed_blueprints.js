83a280f1307159a2ab569869177d42104991c675
Crowdbotics <team@crowdbotics.com>
2020-09-16 03:36:15 +0000

    add package blueprint-react-native-settings-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Settings102004', human_name: 'Settings', access_route: 'Settings102004'},
 { name: 'Tutorial7101999', human_name: 'Tutorial 7', access_route: 'Tutorial7101999', icon: 'question-circle'},
 { name: 'SignUp19101998', human_name: 'Sign Up 1 9', access_route: 'SignUp19101998'},
 { name: 'SignUp110101997', human_name: 'Sign Up 1 10', access_route: 'SignUp110101997'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Settings102004Navigator from '../features/Settings102004/navigator';
 import Tutorial7101999Navigator from '../features/Tutorial7101999/navigator';
 import SignUp19101998Navigator from '../features/SignUp19101998/navigator';
 import SignUp110101997Navigator from '../features/SignUp110101997/navigator';
@@ -19,6 +20,7 @@ import BlankScreen0101994Navigator from '../features/BlankScreen0101994/navigato
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Settings102004: { screen: Settings102004Navigator },
 Tutorial7101999: { screen: Tutorial7101999Navigator },
 SignUp19101998: { screen: SignUp19101998Navigator },
 SignUp110101997: { screen: SignUp110101997Navigator },
